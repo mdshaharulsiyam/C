6083247e2323189909678644cf0ff6d21b70a5fb
mdshaharulsiyam <shaharulsiyam56@gmail.com>
2025-10-08 22:08:27 -0700

    practice 2

---
+++ practiceProblem2/PrintNtime.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  if (n > 0)
+  {
+    for (int i = 0; i < n; i++)
+    {
+      printf("I Love Practice\n");
+    }
+  }
+
+  return 0;
+}
