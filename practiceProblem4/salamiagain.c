bb7cfa1c851e6e5261e31fa29cc291ae84bfdd4d
mdshaharulsiyam <shaharulsiyam56@gmail.com>
2025-10-29 11:31:39 +0600

    s

--- practiceProblem4/salamiagain.c
+++ practiceProblem4/salamiagain.c
@@ -8,6 +8,15 @@ int main()
   for (int i = 0; i < n; i++)
   {
     scanf("%d", &arr[i]);
+    if (max < arr[i])
+    {
+      max = arr[i];
+    }
+  }
+  for (int i = 0; i < n; i++)
+  {
+    arr[i] = max - arr[i];
+    printf("%d ", arr[i]);
   }
   return 0;
 }
